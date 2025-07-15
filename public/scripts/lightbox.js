03c6c75c922608b3485c57fd16da39680ef2194b
Joshua I <joku@Mac.lan>
2025-07-15 16:58:13 -0700

    update lightbox css and animations

--- public/scripts/lightbox.js
+++ public/scripts/lightbox.js
@@ -1,22 +1,27 @@
 window.openLightbox = (startIdx = 0) => {
-  const urls = Array.from(
-    document.querySelectorAll('[data-gallery-img]')
-  ).map((img) => img.src);
+    const urls = Array.from(
+        document.querySelectorAll('[data-gallery-img]')
+    ).map((img) => img.src);
 
-  const dlg = document.getElementById('lightbox');
-  const pic = dlg.querySelector('img');
-  let idx = startIdx;
+    const dlg = document.getElementById('lightbox');
+    const pic = dlg.querySelector('img');
+    let idx = startIdx;
 
-  const show = () => (pic.src = urls[idx]);
-  dlg.querySelector('[data-next]').onclick = () => {
-    idx = (idx + 1) % urls.length;
-    show();
-  };
-  dlg.querySelector('[data-prev]').onclick = () => {
-    idx = (idx - 1 + urls.length) % urls.length;
-    show();
-  };
+    const show = () => (pic.src = urls[idx]);
 
-  show();
-  dlg.showModal();
+    // navigation
+    dlg.querySelector('[data-next]').onclick = () => {
+        idx = (idx + 1) % urls.length;
+        show();
+    };
+    dlg.querySelector('[data-prev]').onclick = () => {
+        idx = (idx - 1 + urls.length) % urls.length;
+        show();
+    };
+
+    show(); // set the src FIRST
+    if (!dlg.open) {
+        // only call showModal the first time
+        dlg.showModal();
+    }
 };
